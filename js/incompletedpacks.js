d55a9e98a1d2db4e7c2afc66e78fed9b112e51c7
zis76 <76632633+zis76@users.noreply.github.com>
2026-01-31 20:21:05 -0600

    Update incompletedpacks.js

--- js/incompletedpacks.js
+++ js/incompletedpacks.js
@@ -1,81 +1,77 @@
     Ship Pack
     if (rank = 1) {
-        return 200;
-    }
-    Ship Pack II
-    if (rank = 2) {
         return 500;
     }
     Wave Pack
-    if (rank = 3) {
+    if (rank = 2) {
         return 350;
     }
     Cube Pack
-    if (rank = 4) {
-        return 250;
+    if (rank = 3) {
+        return 300;
     }
     Robot Pack
-    if (rank = 5) {
+    if (rank = 4) {
         return 650;
     }
     Swing Pack
-    if (rank = 6) {
+    if (rank = 5) {
         return 300;
     }
     Balls Pack
-    if (rank = 7) {
+    if (rank = 6) {
         return 550;
     }
     Daul Pack
-    if (rank = 8) {
+    if (rank = 7) {
         return 400;
     }
-    Consistency Pack I
-    if (rank = 9) {
-        return 150;
-    }
-    Consistency Pack II
-    if (rank = 10) {
-        return 350;
+    Consistency Pack
+    if (rank = 8) {
+        return 500;
     }
     2 Player Pack
-    if (rank = 11) {
+    if (rank = 9) {
         return 700;
     }
     Memory Trials
-    if (rank = 12) {
+    if (rank = 10) {
         return 300;
     }
     Hank Pack
-    if (rank = 13) {
+    if (rank = 11) {
         return 350;
     }
     Anatomy Pack
-    if (rank = 14) {
+    if (rank = 12) {
         return 500;
     }
     Hazbin Hotel
-    if (rank = 15) {
+    if (rank = 13) {
         return 500;
     }
     Hazbin Resort
-    if (rank = 16) {
+    if (rank = 14) {
         return 750;
     }
     Impossible Endings Pack
-    if (rank = 17) {
+    if (rank = 15) {
         return 650;
     }
     Finni Pack
-    if (rank = 18) {
+    if (rank = 17) {
         return 450;
     }
     Decoration Pack I
-    if (rank = 19) {
+    if (rank = 18) {
         return 600;
     }
     Decoration Pack II
-    if (rank = 20) {
+    if (rank = 19) {
         return 1000;
+    }
+    Jack Of All Trades
+    if (rank = 20) {
+        return 1500;
     }
 }
