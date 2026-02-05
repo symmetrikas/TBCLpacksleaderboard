fef1a6c432ad5f3f354630d325064a8467d146dd
zis76 <76632633+zis76@users.noreply.github.com>
2026-02-05 12:14:10 -0600

    Update score.js

--- js/score.js
+++ js/score.js
@@ -11,50 +11,69 @@ const scale = 3;
  * @returns {Number}
  */
 export function score(rank, percent, minPercent) {
+    //Ship Pack
     if (rank == 1) {
         return 500;
     }
+    //Wave Pack
     if (rank == 2) {
         return 350;
     }
+    //Cube Pack
     if (rank == 3) {
         return 300;
     }
+    //Robot Pack
     if (rank == 4) {
         return 650;
     }
+    //Swing Pack
     if (rank == 5) {
         return 300;
     }
+    //Balls Pack
     if (rank == 6) {
         return 550;
     }
+    //Daul Pack
     if (rank == 7) {
         return 400;
     }
+    //Memory Trials
     if (rank == 8) {
         return 300;
     }
+    //Hank Pack
     if (rank == 9) {
         return 350;
     }
+    //Anatomy Pack
     if (rank == 10) {
         return 500;
     }
+    //Hazbin Hotel
     if (rank == 11) {
         return 500;
     }
+    //Hazbin Resort
     if (rank == 12) {
         return 750;
     }
+    //Impossable Endings Pack
     if (rank == 13) {
         return 650;
     }
+    //Finni Pack
     if (rank == 14) {
         return 450;
     }
+    //Deco Pack 1
     if (rank == 15) {
         return 500;
+    }
+    // MP VC Pack
+    if (rank == 16) {
+        return 100;
     }
 }
 
